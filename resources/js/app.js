8feafe4fa90621bb63241bd9bbbce051d035e504
Tom DEPUSSAY <depussay.tom1@gmail.com>
2025-06-11 08:57:18 +0200

    Event for not submit form

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,12 @@ import './modal';
 
 import Alpine from 'alpinejs';
 
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter' && event.target.tagName === 'INPUT') {
+        event.preventDefault();
+    }
+});
+
 window.Alpine = Alpine;
 
 Alpine.start();

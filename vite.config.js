a17e22b11907cc99e206c48136258d654990965c
Moha-ak15 <mosman@myges.fr>
2025-07-15 13:38:39 +0200

    Fichiers JS mis à jour pour la prod

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,26 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     server: {
-        host: '0.0.0.0',
+        host: "0.0.0.0",
         port: 5173,
         hmr: {
-            host: 'localhost',
+            host: "localhost",
         },
-	cors: true,
+        cors: true,
     },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                "resources/css/app.css",
+
+                // Fichier JS
+                "resources/js/app.js",
+                "resources/js/pages/project-show.js",
+                "resources/js/pages/project-create.js",
+                "resources/js/pages/dashboard.js",
+            ],
             refresh: true,
         }),
     ],
